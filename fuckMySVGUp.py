1a39fcf26fd9b90a88e4f5fb454ade256c9a0f35
Dan Hett <hellodanhett@gmail.com>
2016-05-14 18:23:04 +0100

    Added input checking for source SVG

--- fuckMySVGUp.py
+++ fuckMySVGUp.py
@@ -10,17 +10,24 @@
 # python myFile.svg output 50 NORMAL
 # would make fifty versions, at normal intensity, and write them into 'output'
 #
-# Input file = the SVG you want to fuck up
-# Output dir = the directory to spit out into (defaults to current directory)
-# Copies = how many broken versions you want to output
-# Intensity = how broken you want stuff. Can be LOW, NORMAL, HIGH, LUDICROUS, and OMG
+# Input file = the SVG you want to fuck . Required.
+# Output dir = the directory to spit out into. Default: current directoy.
+# Copies = how many broken versions you want to output. Default: one.
+# Intensity = Can be LOW, NORMAL, HIGH, LUDICROUS, and OMG. Default: Normal.
 
 import sys
+import os.path
 
-input = sys.argv[1:][0]
+if os.path.isfile(sys.argv[1:][0]) == True:
+    input = sys.argv[1:][0]
+else:
+    print("Error: Input SVG not found!")
+    sys.exit()
+
 output = sys.argv[1:][1]
 copies = sys.argv[1:][2]
+intensity = sys.argv[1:][3]
 
-print("----------------------------------------------------")
-print("OK! Fucking up " + input + " " + copies + " time(s)")
+print("--------- FUCK MY SVG UP ---------")
+print("OK! Fucking up " + input + " " + copies + " time(s) using " + intensity + " intensity!")
 print("Fucked-up files will be written to: " + output)
